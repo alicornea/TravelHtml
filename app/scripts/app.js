89fb349768dacd2bba47ef1498434fdf139cefa9
alicornea <a_l_i_cornea@yahoo.com>
2015-01-23 16:47:19 +0200

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name travelHtmlApp
+ * @description
+ * # travelHtmlApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('travelHtmlApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name travelHtmlApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the travelHtmlApp
+ */
+angular.module('travelHtmlApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
