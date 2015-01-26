6b41cc3a460d0d7dfeb5f8420b6ce64606d65b25
Alejandro <a_l_i_cornea@yahoo.com>
2015-01-26 09:11:18 +0000

    a

---
+++ app/scripts/controllers/app.js.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name travelHtmlApp.controller:AppJsCtrl
+ * @description
+ * # AppJsCtrl
+ * Controller of the travelHtmlApp
+ */
+angular.module('travelHtmlApp')
+  .controller('AppJsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
