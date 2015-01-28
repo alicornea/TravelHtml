4e20a9117e25c8736b3349e61fa00d1ae593831e
Andrei Murgu <alexandru.murgu@yahoo.com>
2015-01-28 15:17:38 +0000

    add travel view and controller

--- app/scripts/controllers/travel/addtravel.js
+++ app/scripts/controllers/travel/addtravel.js
@@ -8,10 +8,38 @@
  * Controller of the travelHtmlApp
  */
 angular.module('travelHtmlApp')
-  .controller('AddTravelCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('AddTravelCtrl', ['$scope', function($scope) {
+    
+    $scope.visitedPlaces = [];
+
+    $scope.addNewVisitedPlace = function(visitedPlace) {
+      if (visitedPlace !== undefined) {
+        $scope.visitedPlaces.push(visitedPlace);
+        visitedPlace = {
+          name: null,
+          review: null,
+          rating: null
+        };
+      }
+    };
+
+    $scope.submit = function(){
+      $scope.visitedPlaces.push({
+          name: $scope.visitedPlace.name,
+          review: $scope.visitedPlace.review,
+          rating: $scope.visitedPlace.rating
+        });
+      
+      var travel = {
+        location: $scope.locationName,
+        startDate: $scope.startDate,
+        endDate: $scope.endDate,
+        flight: $scope.flight,
+        review: $scope.travelReview,
+        rating: $scope.travelRating,
+        visitedPlaces: $scope.visitedPlaces
+      };
+      console.log(travel);
+    };
+
+  }]);
