7eb1f2b3941052c88e94157ee047d1bd1c73dee3
alicornea <a_l_i_cornea@yahoo.com>
2015-03-11 15:14:56 +0200

    gigi

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,4 @@
+/// <vs BeforeBuild='build' />
 // Generated on 2015-01-23 using generator-angular 0.10.0
 'use strict';
 
@@ -390,7 +391,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
     if (target === 'dist') {
-      return grunt.task.run(['build', 'connect:dist:keepalive']);
+      return grunt.task.run(['build ', 'connect:dist:keepalive']);
     }
 
     grunt.task.run([
@@ -399,7 +400,7 @@ module.exports = function (grunt) {
       'concurrent:server',
       'autoprefixer',
       'connect:livereload',
-      'watch'
+      'watch'      
     ]);
   });
 
